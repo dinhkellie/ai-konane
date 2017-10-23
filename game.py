39ed24e2899884a9434b9d42f52df410a341c306
Kellie Dinh <dinhkellie@yahoo.com>
2017-10-23 09:58:46 -0400

    print board

---
+++ game.py
@@ -0,0 +1,48 @@
+class Konane:
+	def __init__(self, n):
+		self.size = n
+		self.start()
+
+	# Start state of board
+	def start(self):
+		self.board = []
+		value = 'X'
+		for i in range(self.size):
+			row = []
+			for j in range(self.size):
+				row.append(value)
+				value = self.opponent(value)
+			self.board.append(row)
+			if self.size % 2 == 0:
+				value = self.opponent(value)
+
+	# Given symbol, returns opponent's symbol either 'X' or 'O'	
+	def opponent(self, symbol):
+		if symbol == 'X':
+			return 'O'
+		else:
+			return 'X'
+
+	def __str__(self):
+		return self.boardToString(self.board)
+
+	# Return string representation of board
+	def boardToString(self, board):
+		result = "  "
+		for i in range(self.size):
+			result += str(i) + " "
+		result += "\n"
+		for i in range(self.size):
+			result += str(i) + " "
+			for j in range(self.size):
+				result += str(board[i][j]) + " "
+			result += "\n"
+		return result
+
+	def play(self, n):
+		self.start()
+		for i in range (0, n):
+			print self
+
+game = Konane(8)
+game.play(1)
